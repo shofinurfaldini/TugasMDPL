93a414bbe9111adc9ac435e7c258511ce230e3d5
Shofi Isnaeni Nurfaldini <shofinurfaldini@gmail.com>
2021-11-13 00:05:53 +0700

    Menambah Halaman Baru

--- app/src/main/java/com/mws/tugasmdpl/Detail.kt
+++ app/src/main/java/com/mws/tugasmdpl/Detail.kt
@@ -1,11 +1,25 @@
 package com.mws.tugasmdpl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class Detail : AppCompatActivity() {
+class Detail : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnTambah : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        btnTambah = findViewById(R.id.btn_tambah)
+        btnTambah.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        val intent = Intent(this, Pembayaran :: class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/mws/tugasmdpl/Pembayaran.kt
@@ -0,0 +1,11 @@
+package com.mws.tugasmdpl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pembayaran : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pembayaran)
+    }
+}
